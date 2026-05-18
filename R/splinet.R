23e29b87d4c56c95ffad38fe7e6d94dcd7cbcb68
J. Derek Tucker <jdtuck@sandia.gov>
2026-05-18 12:16:09 -0600

    remove splinets as not ready for CRAN

--- R/splinet.R
+++ R/splinet.R
@@ -17,30 +17,30 @@ Knots_prepare <- function(selected_knots, Time) {
 }
 
 
-basisSplinet <- function(Ystandard, nBasis) {
-  nMV = ncol(Ystandard)
-  is_Splinets_available <- requireNamespace("Splinets", quietly = TRUE)
-  if (!is_Splinets_available) {
-    stop("For basisType=='splinets', install Splinets")
-  }
-  is_DDK_available <- requireNamespace("DDK", quietly = TRUE)
-  if (!is_DDK_available) {
-    stop(
-      "For basisType=='splinets', install DDK (devtools::install_github('ranibasna/ddk'))"
-    )
-  }
-  # knot selection
-  initial_knots <- c(0, nMV)
-  # selecting the knots with add_knots function
-  KS <- DDK::add_knots(f = Ystandard,
-                       knots = initial_knots,
-                       L = 20,
-                       M = 1)
-  DDKnots <- Knots_prepare(selected_knots = KS[[1]],
-                           Time = seq(0, 1, length.out = nMV))
-  f_ready_data <- data_prepare(f_data = Ystandard, t_data = seq(0, 1, length.out = nMV))
-  ProjObj <- Splinets::project(f_ready_data, DDKnots)
-  basis = t(Splinets::evspline(ProjObj$basis, x = seq(0, 1, length.out = nMV))[, -1])
-
-  return(basis)
-}
+# basisSplinet <- function(Ystandard, nBasis) {
+#   nMV = ncol(Ystandard)
+#   is_Splinets_available <- requireNamespace("Splinets", quietly = TRUE)
+#   if (!is_Splinets_available) {
+#     stop("For basisType=='splinets', install Splinets")
+#   }
+#   is_DDK_available <- requireNamespace("DDK", quietly = TRUE)
+#   if (!is_DDK_available) {
+#     stop(
+#       "For basisType=='splinets', install DDK (devtools::install_github('ranibasna/ddk'))"
+#     )
+#   }
+#   # knot selection
+#   initial_knots <- c(0, nMV)
+#   # selecting the knots with add_knots function
+#   KS <- DDK::add_knots(f = Ystandard,
+#                        knots = initial_knots,
+#                        L = 20,
+#                        M = 1)
+#   DDKnots <- Knots_prepare(selected_knots = KS[[1]],
+#                            Time = seq(0, 1, length.out = nMV))
+#   f_ready_data <- data_prepare(f_data = Ystandard, t_data = seq(0, 1, length.out = nMV))
+#   ProjObj <- Splinets::project(f_ready_data, DDKnots)
+#   basis = t(Splinets::evspline(ProjObj$basis, x = seq(0, 1, length.out = nMV))[, -1])
+#
+#   return(basis)
+# }
